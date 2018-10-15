c5f206b5c1d190691f6207cc555db41fafd3a3b4
AravindVasudev <aravindv96@yahoo.com>
2018-10-15 07:24:52 +0530

    Added a bit of css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng2048';
+  board: number[][];
+  empty = 0;
+
+  constructor() {
+    // Init board
+    this.fillBoard(4);
+  }
+
+  // create a empty board of specified dimension
+  fillBoard(dimension: number, value = this.empty): void {
+    this.board = new Array(dimension);
+    for (let i = 0; i < dimension; i++) {
+      this.board[i] = new Array(dimension).fill(value);
+    }
+  }
 }
