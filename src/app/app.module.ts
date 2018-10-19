9a26bf855fcfa2ea6194c948b16ad482cefb8777
AravindVasudev <aravindv96@yahoo.com>
2018-10-19 20:11:20 +0530

    Added swipe gesture support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import * as Hammer from 'hammerjs';
+
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any> {
+    'swipe': { direction: Hammer.DIRECTION_ALL }
+  }
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +18,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+    provide: HAMMER_GESTURE_CONFIG,
+    useClass: MyHammerConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
