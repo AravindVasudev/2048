7b11f77d1744a0ad7e0be02a5c057706d85560ac
AravindVasudev <aravindv96@yahoo.com>
2018-10-20 19:39:12 +0530

    Added simple expectimax bot

--- src/app/bot.ts
+++ src/app/bot.ts
@@ -6,10 +6,13 @@ export class Bot {
     active = false;
     botLoop: any;
     speed = 5;
-    heuristicMatrix = [[7, 6, 5, 4],
-                       [6, 5, 4, 3],
-                       [5, 4, 3, 2],
-                       [4, 3, 2, 1]];
+    depth = 5;
+    botPlayer = 0;
+    boardPlayer = 1;
+    heuristicMatrix = [[10, 8, 7, 6],
+                       [8, 7, 6, 5],
+                       [7, 6, 5, 4],
+                       [6, 5, 4, 2]];
 
     constructor(app: AppComponent) {
         this.app = app;
@@ -36,29 +39,75 @@ export class Bot {
         return score;
     }
 
+    areBoardsEqual(board1: number[][], board2: number[][]): boolean {
+        return JSON.stringify(board1) === JSON.stringify(board2);
+    }
+
+    // returns [score, direction]
+    // 0 -> left
+    // 1 -> right
+    // 2 -> up
+    // 3 -> down
+    expectimax(game: Game, prevGame: Game = null, dir: number = -1, player = this.botPlayer, depth = this.depth): [number, number] {
+        if (depth <= 0) {
+            return [this.computeFitness(game.board), dir];
+        }
+
+        if (game.isGameOver() || (prevGame !== null && this.areBoardsEqual(game.board, prevGame.board))) {
+            return [-9999 / depth, dir];
+        }
+
+        if (player === this.boardPlayer) {
+            let score = 0;
+            let numEmpty = 0;
+
+            for (let i = 0; i < game.board.length; i++) {
+                for (let j = 0; j < game.board.length; j++) {
+                    if (game.board[i][j] === game.empty) {
+                        let newGame = new Game().useBoard(this.app.game.board);
+
+                        newGame.board[i][j] = 2;
+                        score += 0.9 * this.expectimax(newGame, game, dir, this.botPlayer, depth - 1)[0];
+
+                        newGame.board[i][j] = 4;
+                        score += 0.1 * this.expectimax(newGame, game, dir, this.botPlayer, depth - 1)[0];
+
+                        numEmpty++;
+                    }
+                }
+            }
+
+            return [score / numEmpty, dir];
+        } else {
+            let newGames: Game[] = [];
+            for (let i = 0; i < 4; i++) {
+                newGames.push(new Game().useBoard(this.app.game.board));
+            }
+
+            newGames[0].slideLeft(true);
+            newGames[1].slideRight(true);
+            newGames[2].slideUp(true);
+            newGames[3].slideDown(true);
+
+            let maxMove: [number, number] = [Number.MIN_SAFE_INTEGER, -1];
+            for (let i = 0; i < 4; i++) {
+                let curMove = this.expectimax(newGames[i], game, i, this.boardPlayer, depth - 1);
+                if (maxMove[0] < curMove[0]) {
+                    maxMove = curMove;
+                }
+            }
+
+            return maxMove;
+        }
+    }
+
     move(): void {
         // debugger;
-        let newGames: Game[] = [];
-        for (let i = 0; i < 4; i++) {
-            newGames.push(new Game().useBoard(this.app.game.board));
-        }
+        let maxMove = this.expectimax(this.app.game);
 
-        newGames[0].slideLeft(true);
-        newGames[1].slideRight(true);
-        newGames[2].slideUp(true);
-        newGames[3].slideDown(true);
+        console.log(maxMove);
 
-        let maxScore = Number.MIN_SAFE_INTEGER;
-        let maxMove: number;
-        for (let i = 0; i < 4; i++) {
-            let curFitness = this.computeFitness(newGames[i].board);
-            if (maxScore < curFitness) {
-                maxScore = curFitness;
-                maxMove = i;
-            }
-        }
-
-        switch (maxMove) {
+        switch (maxMove[1]) {
             case 0:
                 this.app.slideLeft(null);
                 break;
